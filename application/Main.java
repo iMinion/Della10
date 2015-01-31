0c7dd7e8398c3afd36d8d77bf268f7cad847530f
iMinion <k.srirammanoj@gmail.com>
2015-01-31 10:18:31 +0530

    fixed the bug in the link up of "network notifier"

--- application/Main.java
+++ application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		stage = primaryStage;
 		try {
-			Pane root = (Pane) FXMLLoader.load(getClass().getResource("../Layout/appPane.fxml"));
+			Pane root = (Pane) FXMLLoader.load(getClass().getResource("../Layout/Login.fxml"));
 			Scene scene = new Scene(root,657,580);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
